fcf52de71384dabb078b41130c1c594db1890048
Thodoris Adamantidis <thodoris.adamantidis@sitecore.com>
2023-09-24 10:15:37 +0300

    Added TransactionsCSV model

---
+++ TransactionsAPI/Models/TransactionsCSV.cs
@@ -0,0 +1,14 @@
+﻿namespace TransactionsAPI.Models
+{
+    public class TransactionsCSV
+    {
+        public Guid Id { get; set; }
+        public string ApplicationName { get; set; }
+        public string Email { get; set; }
+        public string Filename { get; set; }
+        public Uri Url { get; set; }
+        public DateTime Inception { get; set; }
+        public string Amount { get; set; }
+        public decimal Allocation { get; set; }
+    }
+}
